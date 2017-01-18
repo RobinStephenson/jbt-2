fd29d4a2a9b8b4edebdc4913caec9d3efc0bd8fb
joseph-anderson <jwa509@york.ac.uk>
2017-01-18 12:25:28 +0000

    Changes made according to review, not sure if indentation is ok since it had all looked fine when I was editing it but then went all weird when committed, this now looks ok on Atom so fingers crossed. Removed StartGame (functionality achieved in constructor) and PlayerReadyCheck since we're taking a different appriach to that now.

--- Assets/Classes/Game Manager/GameHandler.cs
+++ Assets/Classes/Game Manager/GameHandler.cs
@@ -6,27 +6,27 @@ using System.collections.generic;
 
 public static class GameHandler
 {
-    public static GameManager CreateNew(string gameName, List<int> players)
+  public static GameManager CreateNew(string gameName, List<int> players)
 	{
-      return new GameManager(gameName, players);
-    }
+    return new GameManager(gameName, players);
+  }
 
-    public static void Save(GameManager gameManagerToSave, string filePath)
-    {
-      Stream stream = File.Open(filePath, FileMode.Create);
-      BinaryFormatter formatter = new BinaryFormatter();
+  public static void Save(GameManager gameManagerToSave, string filePath)
+  {
+    Stream stream = File.Open(filePath, FileMode.Create);
+    BinaryFormatter formatter = new BinaryFormatter();
 
-      formatter.Serialize(stream, gameManagerToSave);
-      stream.Close();
-    }
+    formatter.Serialize(stream, gameManagerToSave);
+    stream.Close();
+  }
 
-    public static GameManager Load(string filePath)
-    {
-      stream = File.Open(filePath, FileMode.Open);
-      BinaryFormatter formatter = new BinaryFormatter();
-      GameManager returnedGameManager = (GameManager)formatter.Deserialize(stream);
-      stream.Close();
+  public static GameManager Load(string filePath)
+  {
+    stream = File.Open(filePath, FileMode.Open);
+    BinaryFormatter formatter = new BinaryFormatter();
+    GameManager returnedGameManager = (GameManager)formatter.Deserialize(stream);
+    stream.Close();
 
-      return returnedGameManager;
-    }
+    return returnedGameManager;
+  }
 }
--- Assets/Classes/Game Manager/GameManager.cs
+++ Assets/Classes/Game Manager/GameManager.cs
@@ -6,46 +6,37 @@ using System;
 [Serializable]
 public class GameManager : System.Object
 {
-    public string gameName;
-    private List<Player> players;
+  public string gameName;
+  private List<Player> players;
 	private int currentPlayer;
-	
+
 	private Market market;
 	private RandomEventFactory factory;
 	private Map map;
-	
-    enum States : int { ACQUISITION, PURCHASE, INSTALLATION,
-       PRODUCTION, AUCTION };
+
+  enum States : int
+  {
+    ACQUISITION, PURCHASE, INSTALLATION, PRODUCTION, AUCTION
+  };
 	private States currentState = States.ACQUISITION;
-		
-    public GameManager(string gameName, List<Player> players)
-    {
-        this.gameName = gameName;
-        this.players = players;
+
+  public GameManager(string gameName, List<Player> players)
+  {
+    this.gameName = gameName;
+    this.players = players;
 		this.market = new Market(new ResourceGroup(100, 100, 100), new ResourceGroup(100, 100, 100), 200);
 		this.randomEventFactory = new RandomEventFactory();
 		this.map = new Map();
-    }
+  }
 
-    public void NextState(){
-		currentState++;
+  public void NextState()
+  {
+	   currentState++;
 	}
-		
-    bool PlayerReadyCheck()
-    {
-        // Check that all players have finished their tasks, or time has
-        // elapsed. Return true if ok to proceed.
-        return false;
-    }
 
-    public void StartGame()
-    {
-        //TODO - Initialise game
-    }
-	
 	public void PlayerAct()
 	{
-		//Check that the current player exists, if not then we have iterated through all players and need to move on to the next stage.
+	   //Check that the current player exists, if not then we have iterated through all players and need to move on to the next stage.
 		if (currentPlayer >= players.Count)
 		{
 			//If we've moved on to the production phase then run the function that handles the logic for the production phase.
@@ -53,23 +44,23 @@ public class GameManager : System.Object
 			{
 				ProcessProductionPhase();
 			}
-		
+
 			currentState++;
 		}
-		
+
 		//Call the Act function for the current player, passing the state to it.
 		players[currentPlayer].Act(currentState);
 		currentPlayer++;
 	}
 
 	private Player GetWinnerIfGameHasEnded()
-	{	
+	{
 		//Game ends if there are no remaining tiles (Req 2.3.a)
 		if(map.GetRemainingTiles() == 0)
 		{
 			highestScore = Mathf.NegativeInfinity;
 			winner = Mathf.Infinity;
-			
+
 			for(int i = 0; i < players.Count; i++)
 			{
 				//Player with the highest score wins (Req 2.3.c)
@@ -79,7 +70,7 @@ public class GameManager : System.Object
 					winner = i;
 				}
 			}
-			
+
 			if(highestScore != Mathf.NegativeInfinity)
 			{
 				return players[winner];
@@ -87,31 +78,31 @@ public class GameManager : System.Object
 			else
 			{
 				return null;
-			}	
+			}
 		}
 	}
-	
+
 	private void ShowWinner(Player player)
 	{
 		//Handle exiting the game, showing a winner screen (leaderboard) and returning to main menu
 	}
-	
-    public void ProcessProductionPhase()
-    {		
-		winner = GetWinnerIfGameHasEnded();
-		if(winner != null)
-		{
-			ShowWinner(winner);
-			return;
-		}
-		
-		for(int i = 0; i < players.Count; i++)
-		{
-			players[i].Produce();
-		}
-		
+
+  public void ProcessProductionPhase()
+  {
+	  winner = GetWinnerIfGameHasEnded();
+	  if(winner != null)
+	    {
+        ShowWinner(winner);
+			  return;
+		  }
+
+		  for(int i = 0; i < players.Count; i++)
+		  {
+			  players[i].Produce();
+		  }
+
 		//Instantiate a random event (probability handled in the randomEventFactory) (Req 2.5.a, 2.5.b)
 		randomEventFactory.Create(Random.Range(0, 101)).Instantiate();
 		market.UpdatePrices();
-    }
+  }
 }
