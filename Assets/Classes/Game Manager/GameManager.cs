46330dc4d5af88432eb3784ee9ec3c7ce06763a4
joseph-anderson <jwa509@york.ac.uk>
2017-01-18 13:40:14 +0000

    Hopefully this has fixed the indentation problems, went through the files in XCode

--- Assets/Classes/Game Manager/GameManager.cs
+++ Assets/Classes/Game Manager/GameManager.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿cusing UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
@@ -6,37 +6,38 @@ using System;
 [Serializable]
 public class GameManager : System.Object
 {
-  public string gameName;
-  private List<Player> players;
-	private int currentPlayer;
+    public string gameName;
+    private List<Player> players;
+    private int currentPlayer;
 
-	private Market market;
+    private Market market;
 	private RandomEventFactory factory;
 	private Map map;
 
-  enum States : int
-  {
-    ACQUISITION, PURCHASE, INSTALLATION, PRODUCTION, AUCTION
-  };
-	private States currentState = States.ACQUISITION;
+    enum States : int
+    {
+        ACQUISITION, PURCHASE, INSTALLATION, PRODUCTION, AUCTION
+    };
 
-  public GameManager(string gameName, List<Player> players)
-  {
-    this.gameName = gameName;
-    this.players = players;
+    private States currentState = States.ACQUISITION;
+
+    public GameManager(string gameName, List<Player> players)
+    {
+        this.gameName = gameName;
+        this.players = players;
 		this.market = new Market(new ResourceGroup(100, 100, 100), new ResourceGroup(100, 100, 100), 200);
 		this.randomEventFactory = new RandomEventFactory();
 		this.map = new Map();
-  }
+    }
 
-  public void NextState()
-  {
+    public void NextState()
+    {
 	   currentState++;
 	}
 
 	public void PlayerAct()
 	{
-	   //Check that the current player exists, if not then we have iterated through all players and need to move on to the next stage.
+        //Check that the current player exists, if not then we have iterated through all players and need to move on to the next stage.
 		if (currentPlayer >= players.Count)
 		{
 			//If we've moved on to the production phase then run the function that handles the logic for the production phase.
@@ -48,7 +49,7 @@ public class GameManager : System.Object
 			currentState++;
 		}
 
-		//Call the Act function for the current player, passing the state to it.
+        //Call the Act function for the current player, passing the state to it.
 		players[currentPlayer].Act(currentState);
 		currentPlayer++;
 	}
@@ -87,19 +88,19 @@ public class GameManager : System.Object
 		//Handle exiting the game, showing a winner screen (leaderboard) and returning to main menu
 	}
 
-  public void ProcessProductionPhase()
-  {
-	  winner = GetWinnerIfGameHasEnded();
-	  if(winner != null)
-	    {
-        ShowWinner(winner);
-			  return;
-		  }
+    public void ProcessProductionPhase()
+    {
+        winner = GetWinnerIfGameHasEnded();
+        if(winner != null)
+        {
+            ShowWinner(winner);
+            return;
+        }
 
-		  for(int i = 0; i < players.Count; i++)
-		  {
-			  players[i].Produce();
-		  }
+        for(int i = 0; i < players.Count; i++)
+        {
+            players[i].Produce();
+        }
 
 		//Instantiate a random event (probability handled in the randomEventFactory) (Req 2.5.a, 2.5.b)
 		randomEventFactory.Create(Random.Range(0, 101)).Instantiate();
