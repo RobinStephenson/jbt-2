8d009941a6129e423b9c25664959dc7423e5d2e6
Rlindsay24 <richard.lindsay3@googlemail.com>
2017-01-18 14:43:29 +0000

    Review #2
    removed enums and object declarations

--- Assets/Classes/Game Manager/RandomEventStore.cs
+++ Assets/Classes/Game Manager/RandomEventStore.cs
@@ -4,78 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomEventStore
-{   
-    enum regEvents
-    {
-        EARTHQUAKE,
-        VOLCANO,
-        METEORSHOWER,
-        METEORSTRIKE,
-        SOLARFLARE,
-        DROUGHT,
-        FLOOD,
-        CAVERNS,
-        CLEARSKIES,
-        DONATION,
-        DIAMONDS,
-        TECHUPGRADE
-    }
-    enum crazyEvents
-    {
-        THATCHERSREVENGE,
-        ALIENABDUCTION,
-        ALIENGIFTS,
-        STORAGEHUNTER,
-        GOOSEINFESTATION,
-        HANGOVER,
-        BUSLATE,
-        LAUNDRYPRICE,
-        CENTRALHALL,
-        BADPUBLICITY
-    };
-    //Regular Events
-    public GameObject EARTHQUAKE;
-    public GameObject VOLCANO;
-    public GameObject METEORSHOWER;
-    public GameObject METEORSTRIKE;
-    public GameObject SOLARFLARE;
-    public GameObject DROUGHT;
-    public GameObject FLOOD;
-    public GameObject CAVERNS;
-    public GameObject CLEARSKIES;
-    public GameObject DONATION;
-    public GameObject DIAMONDS;
-    public GameObject TECHUPGRADE;
-    //Crazy Events
-    public GameObject THATCHERSREVENGE;
-    public GameObject ALIENABDUCTION;
-    public GameObject ALIENGIFTS;
-    public GameObject STORAGEHUNTER;
-    public GameObject GOOSEINFESTATION;
-    public GameObject HANGOVER;
-    public GameObject BUSLATE;
-    public GameObject LAUNDRYPRICE;
-    public GameObject CENTRALHALL;
-    public GameObject BADPUBLICITY;
+{  
+    public List<GameObject> RegularEvents = new List<GameObject> {};    // TODO: Add GameObjects() for each regular event in Unity editor
+    public List<GameObject> CrazyEvents = new List<GameObject> {};      // TODO: Add GameObjects() for each crazy event in Unity editor
 
-    public List<GameObject> RegularEvents = new List<GameObject> {
-        EARTHQUAKE, VOLCANO, METEORSHOWER, METEORSTRIKE, SOLARFLARE, DROUGHT, FLOOD, CAVERNS, CLEARSKIES, DONATION, DIAMONDS, TECHUPGRADE };
-    public List<GameObject> CrazyEvents = new List<GameObject> {
-        THATCHERSREVENGE, ALIENABDUCTION, ALIENGIFTS, STORAGEHUNTER, GOOSEINFESTATION, HANGOVER, BUSLATE, LAUNDRYPRICE, CENTRALHALL, BADPUBLICITY };
-
-    int REGEVENTSLENGTH = RegularEvents.Count();
-    int CRAZYEVENTSLENGTH = CrazyEvents.Count();
+    int regEventsLength = RegularEvents.Count();
+    int crazyEventsLength = CrazyEvents.Count();
 
     public GameObject chooseEvent(int craziness)
     {
         Random rnd = new Random();
         if (craziness < 50)
         {
-            return this.RegularEvents[rnd.NextInt(REGEVENTSLENGTH)];
+            return this.RegularEvents[rnd.NextInt(REGEVENTSLENGTH)];    // Choose a random event from the RegularEvents list
         }
         else
         {
-            return this.CrazyEvents[rnd.NextInt(CRAZYEVENTSLENGTH)];
+            return this.CrazyEvents[rnd.NextInt(CRAZYEVENTSLENGTH)];    // Choose a random event from the CrazyEvents list
         }
     }
 }
