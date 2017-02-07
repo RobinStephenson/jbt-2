e38ac290b71838b96a83cfe9d9d57007feaa5935
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-02-07 03:40:04 +0000

    Finished Tile tests

--- Assets/Editor/Tests/MapUnitTests.cs
+++ Assets/Editor/Tests/MapUnitTests.cs
@@ -29,77 +29,52 @@ public class MapUnitTests
         Assert.AreEqual(Map.MAP_DIMENSIONS.x * Map.MAP_DIMENSIONS.y, m.GetNumUnownedTilesRemaining());
     }
 
-    
-    private string TileTest()
+    [Test]
+    public void InitialiseTile()
     {
         Tile tile = new Tile(new ResourceGroup(10, 10, 10), new Vector2(3, 3), 7);
-        string errorString = "";
 
-        //test constructor
-        Vector2 pos = tile.GetTileObject().GetTilePosition();
-        if(pos.x != 1 || pos.y != 2)
-        {
-            errorString += (string.Format("Tile position calculated improperly for test 2.1.0.1\nShould have been (1,2) was actually {0}, {1}\n\n", pos.x, pos.y));
-        }
+        Assert.AreEqual(new Vector2(1, 2), tile.GetTileObject().GetTilePosition());
+    }
 
-        //test InstallRoboticon
-        Roboticon roboticon = new Roboticon();
-        tile.InstallRoboticon(roboticon);
+    [Test]
+    public void InstallRoboticon()
+    {
+        Tile tile = new Tile(new ResourceGroup(10, 10, 10), new Vector2(3, 3), 7);
+        Roboticon r = new Roboticon();
+        tile.InstallRoboticon(r);
+        Assert.Throws<System.Exception>(() => tile.InstallRoboticon(r));
+    }
 
-        bool caught = false;
-        try
-        {
-            tile.InstallRoboticon(roboticon);
-        }
-        catch(System.Exception)
-        {
-            caught = true;
-        }
-        finally
-        {
-            if (!caught)
-            {
-                errorString += (string.Format("No exception caught for test 2.1.1.1\nShould have thrown SystemException when trying to place the same Roboticon on the same tile\n\n"));
-            }
-        }
+    [Test]
+    public void UninstallRoboticon()
+    {
+        Tile tile = new Tile(new ResourceGroup(10, 10, 10), new Vector2(3, 3), 7);
+        Roboticon r = new Roboticon();
+        tile.InstallRoboticon(r);
+        tile.UninstallRoboticon(r);
+        Assert.Throws<System.Exception>(() => tile.UninstallRoboticon(r));
+    }
 
-        //test InstallRoboticon
-        Roboticon roboticon2 = new Roboticon();
-        caught = false;
+    [Test]
+    public void GetPriceTest()
+    {
+        Tile tile = new Tile(new ResourceGroup(10, 10, 10), new Vector2(3, 3), 7);
+        int price = tile.GetPrice();
 
-        try
-        {
-            tile.UninstallRoboticon(roboticon2);
-        }
-        catch (System.Exception)
-        {
-            caught = true;
-        }
-        finally
-        {
-            if (!caught)
-            {
-                errorString += ("No exception caught for test 2.1.3.1\nShould have thrown SystemException when trying to place the same Roboticon on the same tile\n\n");
-            }
-        }
+        Assert.AreEqual(300, tile.GetPrice());
+    }
 
-        //test GetPrice
-        Tile tile2 = new Tile(new ResourceGroup(10, 10, 10), new Vector2(3, 3), 7);
-        int price = tile2.GetPrice();
-        if(price != 300)
-        {
-            errorString += (string.Format("Tile price for test 2.1.4.1\nShould read 300, actually reads {0}\n\n", price));
-        }
+    [Test]
+    public void GetTotalResourceGeneratedTest()
+    {
+        Tile tile = new Tile(new ResourceGroup(10, 10, 10), new Vector2(3, 3), 7);
+        Roboticon r = new Roboticon(new ResourceGroup(2, 0, 1));
+        tile.InstallRoboticon(r);
 
-        //test GetTotalResourcesGenerated
-        tile2.InstallRoboticon(new Roboticon());
-        ResourceGroup resources = tile2.GetTotalResourcesGenerated();
-        ResourceGroup actualResources = tile2.GetBaseResourcesGenerated() + tile2.GetInstalledRoboticons()[0].GetUpgrades() * Tile.ROBOTICON_UPGRADE_WEIGHT;
-        if(!resources.Equals(actualResources))
-        {
-            errorString += (string.Format("Amount of resources for 2.1.5.1\nShould read {0}, actually reads {1}\n\n", actualResources.ToString(), resources.ToString()));
-        }
+        ResourceGroup resources = tile.GetTotalResourcesGenerated();
+        ResourceGroup actualResources = tile.GetBaseResourcesGenerated() + tile.GetInstalledRoboticons()[0].GetUpgrades() * Tile.ROBOTICON_UPGRADE_WEIGHT;
 
-        return errorString;
+        Assert.AreEqual(resources, actualResources);
     }
 }
