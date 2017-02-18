fb6b2d9d65d909453558ae4a2395d37c2dadf6cd
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-02-18 20:02:56 +0000

    Changed endgame script and scence to different spelling to allow to switch to camelcase

---
+++ Assets/Code/Scripts/GUI/enndGameScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Made by JBT
+public class endGameScript : MonoBehaviour
+{
+    public static List<ScoreboardEntry> Scoreboard;
+
+    public Text WinnerText;
+    public Text LoserText;
+    public Text WinnerScore;
+    public Text LoserScore;
+
+    void Start()
+    {
+        //Destroy objects that got carried over from the main game
+        Destroy(GameObject.Find("Player GUI Canvas(Clone)"));
+        Destroy(GameObject.Find("GameManager"));
+        Destroy(GameObject.Find("Tile Holder"));
+        Destroy(GameObject.Find("Map Manager"));
+
+        SetScoreBoard();
+    }
+
+
+    /// <summary>
+    /// Script for Play again button. Loads main menu scene
+    /// </summary>
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    /// <summary>
+    /// Quits the application
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Sets the Scoreboard text objects within the EndGameScene
+    /// </summary>
+    public void SetScoreBoard()
+    {
+        WinnerText.text = Scoreboard[1].PlayerName;
+        LoserText.text = Scoreboard[0].PlayerName;
+        WinnerScore.text = ("Score: " + Scoreboard[1].PlayerScore.ToString());
+        LoserScore.text = ("Score: " + Scoreboard[0].PlayerScore.ToString());
+    }
+}
