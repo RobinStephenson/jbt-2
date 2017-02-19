188062cc3666bcfd8491bf89b86cc6e42c64f193
Matthew Turner <Juppaware@gmail.com>
2017-02-19 16:10:20 +0000

    Main Menu now starts a new game

--- Assets/Code/Scripts/GUI/MainMenuScript.cs
+++ Assets/Code/Scripts/GUI/MainMenuScript.cs
@@ -22,26 +22,26 @@ public class mainMenuScript : MonoBehaviour
             Destroy(GameObject.Find("Player GUI Canvas(Clone)"));
 
             DontDestroyOnLoad(this);
-
-            //TODO - BALANCE PLAYER RESOURCES
-            List<Player> players = new List<Player>();
-            if (AIToggle.isOn == true)
-            {
-                players.Add(new Human(new ResourceGroup(50, 999, 50), Player1Name.text, 999));
-                players.Add(new AI(new ResourceGroup(3, 2, 3), "BeepBoop", 500));
-            }
-            else
-            {
-                players.Add(new Human(new ResourceGroup(5, 8, 9), Player1Name.text, 10));
-                players.Add(new Human(new ResourceGroup(50, 999, 50), Player2Name.text, 999));
-            }
-
-
-            GameHandler.CreateNew(gameName, players);
-            GameHandler.GetGameManager().StartGame();
-
-            SceneManager.LoadScene(GAME_SCENE_INDEX);   //LoadScene is asynchronous
         }
+        //TODO - BALANCE PLAYER RESOURCES
+        List<Player> players = new List<Player>();
+        if (AIToggle.isOn == true)
+        {
+            players.Add(new Human(new ResourceGroup(50, 999, 50), Player1Name.text, 999));
+            players.Add(new AI(new ResourceGroup(3, 2, 3), "BeepBoop", 500));
+        }
+        else
+        {
+            players.Add(new Human(new ResourceGroup(5, 8, 9), Player1Name.text, 10));
+            players.Add(new Human(new ResourceGroup(50, 999, 50), Player2Name.text, 999));
+        }
+
+        GameHandler.CreateNew(gameName, players);
+        GameHandler.GetGameManager().StartGame();
+            
+        SceneManager.LoadScene(GAME_SCENE_INDEX);   //LoadScene is asynchronous
+        Destroy(GameObject.Find("mainMenu"));
+        
     }
     public void QuitGame()
     {
@@ -52,14 +52,14 @@ public class mainMenuScript : MonoBehaviour
     {
         if (AIToggle.isOn == true)
         {
-            Player1Name.text = "AI";
-            Player1Name.gameObject.SetActive(false);
-            Player1Name.text = "AI";
+            Player2Name.text = "AI";
+            Player2Name.DeactivateInputField();
+            Player2Name.text = "AI";
         }
         else
         {
-            Player1Name.text = "Enter Name Here...";
-            Player1Name.gameObject.SetActive(true);
+            Player2Name.text = "Enter Name Here...";
+            Player2Name.ActivateInputField();
         }
     }
 }
