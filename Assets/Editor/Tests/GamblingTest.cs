200ad3f3ca81495235fa84847f83f5ed58fe3815
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-02-19 01:37:25 +0000

    Added more gambling tests

--- Assets/Editor/Tests/GamblingTest.cs
+++ Assets/Editor/Tests/GamblingTest.cs
@@ -38,4 +38,18 @@ public class GamblingTest {
         int actualRoll;
         Assert.Throws<System.ArgumentException>(() => m.DoubleOrNothing(10, 49, 49, out actualRoll));
     }
+
+    /// <summary>
+    /// Bit of a weird test to explain. This test insures that the market will still be able to provide gambling if it doesnt have enough money, but will have enough money when the player pays to play.
+    /// For example, the market has £15, a player wants to play. The market can't afford the £20 reward, but it can after recieving payment from the player
+    /// </summary>
+    [Test]
+    public void MarketGetsEnoughMoney()
+    {
+        Market m = new Market();
+        m.SetMoney(15);
+
+        int actualRoll;
+        m.DoubleOrNothing(10, 49, 49, out actualRoll);
+    }
 }
