e546db3704aa7b228f918ea3af3344ceaf15dfb1
Matthew Turner <Juppaware@gmail.com>
2017-02-19 12:41:11 +0000

    Wrote Update market test in market test

--- Assets/Editor/Tests/MarketTest.cs
+++ Assets/Editor/Tests/MarketTest.cs
@@ -61,19 +61,17 @@ public class MarketTest
     public void UpdateMarketPrice()
     {
         Market m = new Market();
-        ResourceGroup expectedBuyPrice = new ResourceGroup((10 / 16) + 5, (10 / 16) + 5, (10 / 0) + 5);
-        resourceBuyingPrices = newBuyPrice;
-
-        //Multiplies the buyprice by the markup to get the new sell price
-        ResourceGroup newSellPrice = newBuyPrice * marketMarkup;
-        resourceSellingPrices = newSellPrice;
+        ResourceGroup expectedBuyPrice = new ResourceGroup(5,5,15);
+        ResourceGroup expectedSellPrice = expectedBuyPrice * 1.1;
 
         //Money when market is initialised = 100, changed to 10.
         m.SetMoney(10);
 
+        //Update the prices to account for changed money
         m.UpdatePrices();
-        Assert.Throws<System.ArgumentException>(() => m.BuyFrom(new ResourceGroup(0, 0, -5), 0));
-        Assert.AreEqual(new ResourceGroup(Market.STARTING_FOOD_AMOUNT, Market.STARTING_ENERGY_AMOUNT, ore), m.GetResources());
+
+        Assert.AreEqual(expectedBuyPrice, m.GetResourceBuyingPrices);
+        Assert.AreEqual(expectedSellPrice, m.GetResourceSellingPrices);
     }
 }	
 
