208bb668cc8977af01ea33e0d49c5d29c19f855c
Matthew Turner <Juppaware@gmail.com>
2017-02-20 13:45:23 +0000

    Added documentation

--- Assets/Code/Scripts/mainMenuDayNightCylce.cs
+++ Assets/Code/Scripts/mainMenuDayNightCylce.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Created by JBT to rotate a direction light in the scene
 public class mainMenuDayNightCylce : MonoBehaviour {
     public Light sun;
-
-    public 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
