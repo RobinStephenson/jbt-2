25d2522371360536d5e4ffb88dfba17b03bd42ed
Matthew Turner <Juppaware@gmail.com>
2017-02-20 12:48:12 +0000

    Fixed issue with unity not recognising main menu script

--- Assets/Code/Scripts/GUI/MainMenuScript.cs
+++ Assets/Code/Scripts/GUI/MainMenuScript.cs
@@ -18,6 +18,7 @@ public class mainMenuScript : MonoBehaviour
 
     public void StartGame()
     {
+        Debug.Log("wew");
         //If a player is quitting to the menu from the game itself, or the end of game screen, then remove the GUI canvas that was not destroyed on load
         if (GameObject.Find("Player GUI Canvas(Clone)") != null)
         {
