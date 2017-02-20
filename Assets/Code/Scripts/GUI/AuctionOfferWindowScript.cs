95fb3e8d17a13d72588022abbb130221d1c698c9
Tom Comben <tc975@york.ac.uk>
2017-02-20 13:30:20 +0000

    Changed the name of the offer window script

--- Assets/Code/Scripts/GUI/AuctionOfferWindowScript.cs
+++ Assets/Code/Scripts/GUI/AuctionOfferWindowScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AauctionOfferWindowScript : MonoBehaviour
+public class auctionOfferWindowScript : MonoBehaviour
 {
     public canvasScript AuctionCanvas;
     private AuctionManager auctionManager;
