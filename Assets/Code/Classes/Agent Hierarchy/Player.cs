c843a5c436850e9ddfd6e74936008962b859405a
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-02-21 10:26:26 +0000

    Commented player class declaration

--- Assets/Code/Classes/Agent Hierarchy/Player.cs
+++ Assets/Code/Classes/Agent Hierarchy/Player.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// A player object, used to represent a player in the game, which can either be a Human or an AI
+/// </summary>
 public abstract class Player : Agent
 {
     public static int PlayerCount;
